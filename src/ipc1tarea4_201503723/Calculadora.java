1d24f3dadea0f7e6464dff65203052e2144beb80
Bryan Lopez <bryansyng2@gmail.com>
2018-03-10 21:26:06 -0600

    Se agregaron toggle buttons y para seleccionar la operacion que se desea realizar, buton igual para mostrar el resultado y tambien un label para mostrar que no se ha seleccionado una operacion si es que todas estan desactivadas

--- src/ipc1tarea4_201503723/Calculadora.java
+++ src/ipc1tarea4_201503723/Calculadora.java
@@ -5,8 +5,12 @@
  */
 package ipc1tarea4_201503723;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 
 /**
  *
@@ -31,10 +35,44 @@ public class Calculadora {
         txtResultado.setBounds(475, 50, 200, 50);
         txtResultado.setEditable(false);
         
+        //togglebuttons para establecer la operacion que se realizara
+        JToggleButton btnSuma = new JToggleButton("+", true);
+        btnSuma.setBounds(75,150,100,100);
+        
+        JToggleButton btnResta = new JToggleButton("-", false);
+        btnResta.setBounds(300,150,100,100);
+        
+        JToggleButton btnMultiplicacion = new JToggleButton("*", false);
+        btnMultiplicacion.setBounds(75,300,100,100);
+        
+        JToggleButton btnDivision = new JToggleButton("/", false);
+        btnDivision.setBounds(300,300,100,100);
+        
+        //boton de igual para saber el resultado
+        JButton btnIgual = new JButton("=");
+        btnIgual.setBounds(525,225,100,100);
+        
+        //label de error por si no esta sellecionado ningun toggle button
+        JLabel lblError = new JLabel("");
+        lblError.setBounds(75, 500, 200, 200);
+        
         //se agregan los componentes a la ventana
         ventana.add(txtNumero1);
         ventana.add(txtNumero2);
         ventana.add(txtResultado);
+        ventana.add(btnSuma);
+        ventana.add(btnResta);
+        ventana.add(btnMultiplicacion);
+        ventana.add(btnDivision);
+        ventana.add(btnIgual);
+        ventana.add(lblError);
+        
+        //se agregan los toggle buttons a un grupo para que solo 1 pueda ser seleccionado a la vez
+        ButtonGroup grupo = new ButtonGroup();
+        grupo.add(btnSuma);
+        grupo.add(btnResta);
+        grupo.add(btnMultiplicacion);
+        grupo.add(btnDivision);
         
         //Tamaño de la ventana
         ventana.setSize(725, 550);
